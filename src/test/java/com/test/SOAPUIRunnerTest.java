ae5bb0cb942fc142af2fcf66278030f2609e4fd5
runapal5 <runapal5@gmail.com>
2017-03-09 11:32:59 +0530

    Update SOAPUIRunnerTest.java

--- src/test/java/com/test/SOAPUIRunnerTest.java
+++ src/test/java/com/test/SOAPUIRunnerTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.eviware.soapui.impl.wsdl.panels.support.MockTestRunner;
+import java.util.*;
 import com.eviware.soapui.impl.wsdl.WsdlProjectPro;
 import com.eviware.soapui.impl.wsdl.WsdlTestSuite;
 import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCase;
@@ -12,63 +14,102 @@ import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCaseRunner;
 import com.eviware.soapui.model.testsuite.TestRunner.Status;
 import com.eviware.soapui.model.testsuite.TestStep;
 import com.eviware.soapui.support.types.StringToObjectMap;
+import com.eviware.soapui.model.testsuite.TestCaseRunContext;
+import com.eviware.soapui.impl.wsdl.WsdlProject; 
+import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCase; 
+import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCaseRunner; 
+import com.eviware.soapui.impl.wsdl.testcase.WsdlTestRunContext; 
+import com.eviware.soapui.model.testsuite.LoadTest; 
+import com.eviware.soapui.model.testsuite.TestCase; 
+import com.eviware.soapui.model.testsuite.TestStep; 
+import com.eviware.soapui.model.testsuite.TestStepResult; 
+import com.eviware.soapui.model.testsuite.TestSuite; 
 
 
 public class SOAPUIRunnerTest {
 
+
+    private ArtifactPublisherListener listener; 
+	
     @Test
     public void test() {
-    	
-    	try {
+		
+		  try { 
+                        String projectFile = "/var/lib/jenkins/workspace/App_Exec_NFRTestNG/Pizzas-soapui-project.xml";
+			WsdlProject project = new WsdlProject(projectFile); 
+			for (TestSuite testSuite : project.getTestSuiteList()) { 
 
-	        SoapUIExtensionTestCaseRunner runner = new  SoapUIExtensionTestCaseRunner();
-	        runner.setOutputFolder("target/reports");
-	        
-	        runner.getLicenseManagerServiceFactory();
-	
-	        // assign folder path of composite soapui project to SOAPUI_CI_PROJECT variable 
-	        
-	        //String SOAPUI_CI_PROJECT= "SOAPUI composite project folder path";
-	
-	        String SOAPUI_CI_PROJECT = "/var/lib/jenkins/workspace/App_Exec_NFRTestNG/Pizzas-soapui-project.xml";
-	
-	        System.out.println("******************************* SOAP UI PROJECT FILE ************* "+SOAPUI_CI_PROJECT);
-	
-	        runner.setProjectFile(SOAPUI_CI_PROJECT);
-	        runner.setPrintReport(true);
-	        runner.setIgnoreErrors(false);
-	        runner.setPrintReport(true);
-	
-	
-	        WsdlProjectPro project = new WsdlProjectPro(SOAPUI_CI_PROJECT);
-	        List<WsdlTestSuite> testSuites = project.getTestSuiteList();
-	
-            for( WsdlTestSuite suite : testSuites ) {
+			System.out.println("TestSuiteProject size......" + project.getTestSuiteList().size()); 
+			System.out.println("TestSuiteProject......" + testSuite.getName()); 
 
-                List<WsdlTestCase> testCases = suite.getTestCaseList();
-                
-                for (WsdlTestCase testCase : testCases) {
-                	
-                	System.out.println("========================== TEST CASE =====================::"+ testCase.getName());
-                	WsdlTestCaseRunner testRunner = new WsdlTestCaseRunner( testCase, new StringToObjectMap());
-					
-					
-                	List<TestStep> testSteps = testCase.getTestStepList();
-                	
-                	for(TestStep testStep: testSteps) {
-                		
-                		System.out.println("--------------------- TEST STEP 1----------------- :"+ testStep.getName());
-                		testRunner.runTestStep(testStep);
-						System.out.println("--------------------- TEST RUNNER STATUS ----------------- :"+ testRunner.getStatus());
-                		if(testRunner.getStatus().equals(Status.FAILED)) {
-                			Assert.fail();
-                		}
-                	}
-                }
-            }
-    	}catch(Exception e) {
-    		e.printStackTrace();
-    	}
-    }
+			for (TestCase testCase : testSuite.getTestCaseList()) { 
 
-}
+				System.out.println("TestCases........." + testCase.getName()); 
+
+
+
+
+				StringToObjectMap properties = new StringToObjectMap(); 
+				//properties.put("username", userName); 
+				//properties.put("password", passWord); 
+				//properties.put("localhost", localHost); 
+				//properties.put("HttpDefaultPort", port); 
+				List<TestStep> testSteps = testCase.getTestStepList(); 
+
+
+				// TestRunner runner = testCase.run(properties, false); 
+				WsdlTestCaseRunner wsdlTestCaseRunner = new WsdlTestCaseRunner((WsdlTestCase) testCase, properties); 
+				WsdlTestRunContext wsdlTestRunContext = wsdlTestCaseRunner.createContext(properties); 
+				wsdlTestCaseRunner.run(); 
+
+
+
+
+
+									
+
+				WsdlTestCase wsdlTestCase = wsdlTestCaseRunner.getTestRunnable(); 
+				System.out.println("Functional test case " + wsdlTestCase); 
+				List<TestStepResult> testStepResult = (List<TestStepResult>) wsdlTestCaseRunner.getResults(); 
+				System.out.println("Test stepResults............." + testStepResult); 
+				System.out.println("Test stepResultsn size ............." + testStepResult.size()); 
+				// System.out.println("Test stepResultsn size ............." + testStepResult.); 
+				Iterator<TestStepResult> testIterator = testStepResult.iterator(); 
+				while (testIterator.hasNext()) { 
+						TestStepResult tsestStepResult = testIterator.next(); 
+						System.out.println("Test step next element ..... " + tsestStepResult.toString()); 
+						//System.out.println("Test step size..... " + tsestStepResult.getSize()); 
+						System.out.println("Test step Status..... " + tsestStepResult.getStatus()); 
+				} 
+
+
+			    /*
+								List<LoadTest> loadtest = wsdlTestCase.getLoadTestList(); 
+								System.out.println("Load test cases " + loadtest); 
+								Iterator<LoadTest> loadIterator = loadtest.iterator(); 
+								System.out.println("Load test size...." + loadtest.size()); 
+
+								while (loadIterator.hasNext()) { 
+										LoadTest loadTest = loadIterator.next(); 
+
+										System.out.println("Load Test " + loadTest); 
+
+								}
+			    */
+								//break; 
+						} 
+
+				    //System.exit(1); 
+				} 
+
+			  } catch (Exception e) { 
+			         e.printStackTrace(); 
+			  } 
+
+	   } 
+						
+	}
+	
+	
+
+
