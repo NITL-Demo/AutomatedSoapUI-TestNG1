33cd17043dcc12758f3fbb4b36e350743837b153
runapal5 <runapal5@gmail.com>
2017-03-09 11:48:23 +0530

    Update SOAPUIRunnerTest.java

--- src/test/java/com/test/SOAPUIRunnerTest.java
+++ src/test/java/com/test/SOAPUIRunnerTest.java
@@ -24,7 +24,7 @@ import com.eviware.soapui.model.testsuite.TestCase;
 import com.eviware.soapui.model.testsuite.TestStep; 
 import com.eviware.soapui.model.testsuite.TestStepResult; 
 import com.eviware.soapui.model.testsuite.TestSuite; 
-
+import com.eviware.soapui.model.testsuite.TestStepResult.TestStepStatus;
 
 public class SOAPUIRunnerTest {
 
@@ -34,7 +34,7 @@ public class SOAPUIRunnerTest {
     @Test
     public void test() {
 		
-		  try { 
+		 try { 
                         String projectFile = "/var/lib/jenkins/workspace/App_Exec_NFRTestNG/Pizzas-soapui-project.xml";
 			WsdlProject project = new WsdlProject(projectFile); 
 			for (TestSuite testSuite : project.getTestSuiteList()) { 
@@ -44,67 +44,50 @@ public class SOAPUIRunnerTest {
 
 			for (TestCase testCase : testSuite.getTestCaseList()) { 
 
-				System.out.println("TestCases........." + testCase.getName()); 
+					System.out.println("TestCases........." + testCase.getName()); 
 
 
+					StringToObjectMap properties = new StringToObjectMap(); 
+					//properties.put("username", userName); 
+					//properties.put("password", passWord); 
+					//properties.put("localhost", localHost); 
+					//properties.put("HttpDefaultPort", port); 
+					List<TestStep> testSteps = testCase.getTestStepList(); 
 
 
-				StringToObjectMap properties = new StringToObjectMap(); 
-				//properties.put("username", userName); 
-				//properties.put("password", passWord); 
-				//properties.put("localhost", localHost); 
-				//properties.put("HttpDefaultPort", port); 
-				List<TestStep> testSteps = testCase.getTestStepList(); 
+					// TestRunner runner = testCase.run(properties, false); 
+					WsdlTestCaseRunner wsdlTestCaseRunner = new WsdlTestCaseRunner((WsdlTestCase) testCase, properties); 
+					WsdlTestRunContext wsdlTestRunContext = wsdlTestCaseRunner.createContext(properties); 
+					wsdlTestCaseRunner.run(); 
+
+			
+
+					WsdlTestCase wsdlTestCase = wsdlTestCaseRunner.getTestRunnable(); 
+					System.out.println("Functional test case " + wsdlTestCase); 
+					List<TestStepResult> testStepResult = (List<TestStepResult>) wsdlTestCaseRunner.getResults(); 
+					System.out.println("Test stepResults............." + testStepResult); 
+					System.out.println("Test stepResultsn size ............." + testStepResult.size()); 
+					// System.out.println("Test stepResultsn size ............." + testStepResult.); 
+					Iterator<TestStepResult> testIterator = testStepResult.iterator(); 
+					while (testIterator.hasNext()) { 
+							TestStepResult tsestStepResult = testIterator.next(); 
+							System.out.println("Test step next element ..... " + tsestStepResult.toString()); 
+							//System.out.println("Test step size..... " + tsestStepResult.getSize()); 
+							System.out.println("Test step Status..... " + tsestStepResult.getStatus()); 
+							Assert.assertEquals( TestStepStatus.OK , tsestStepResult.getStatus());
+							//Assert.assertEquals( TestStepStatus.FAILED , tsestStepResult.getStatus());
+					} 
 
 
-				// TestRunner runner = testCase.run(properties, false); 
-				WsdlTestCaseRunner wsdlTestCaseRunner = new WsdlTestCaseRunner((WsdlTestCase) testCase, properties); 
-				WsdlTestRunContext wsdlTestRunContext = wsdlTestCaseRunner.createContext(properties); 
-				wsdlTestCaseRunner.run(); 
-
-
-
-
-
-									
-
-				WsdlTestCase wsdlTestCase = wsdlTestCaseRunner.getTestRunnable(); 
-				System.out.println("Functional test case " + wsdlTestCase); 
-				List<TestStepResult> testStepResult = (List<TestStepResult>) wsdlTestCaseRunner.getResults(); 
-				System.out.println("Test stepResults............." + testStepResult); 
-				System.out.println("Test stepResultsn size ............." + testStepResult.size()); 
-				// System.out.println("Test stepResultsn size ............." + testStepResult.); 
-				Iterator<TestStepResult> testIterator = testStepResult.iterator(); 
-				while (testIterator.hasNext()) { 
-						TestStepResult tsestStepResult = testIterator.next(); 
-						System.out.println("Test step next element ..... " + tsestStepResult.toString()); 
-						//System.out.println("Test step size..... " + tsestStepResult.getSize()); 
-						System.out.println("Test step Status..... " + tsestStepResult.getStatus()); 
+			  
 				} 
 
+				    
+			    } 
 
-			    /*
-								List<LoadTest> loadtest = wsdlTestCase.getLoadTestList(); 
-								System.out.println("Load test cases " + loadtest); 
-								Iterator<LoadTest> loadIterator = loadtest.iterator(); 
-								System.out.println("Load test size...." + loadtest.size()); 
-
-								while (loadIterator.hasNext()) { 
-										LoadTest loadTest = loadIterator.next(); 
-
-										System.out.println("Load Test " + loadTest); 
-
-								}
-			    */
-								//break; 
-						} 
-
-				    //System.exit(1); 
-				} 
-
-			  } catch (Exception e) { 
-			         e.printStackTrace(); 
-			  } 
+		  } catch (Exception e) { 
+			 e.printStackTrace(); 
+		  } 
 
 	   } 
 						
